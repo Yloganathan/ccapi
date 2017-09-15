93883a3f99f76fe5772364af20283cb6e45d98e7
Yuva <Yloganathan@users.noreply.github.com>
2017-09-14 18:12:10 -0600

    Update LedgerService.java

--- src/main/java/com/capone/ccapi/util/LedgerService.java
+++ src/main/java/com/capone/ccapi/util/LedgerService.java
@@ -8,38 +8,37 @@ import java.util.stream.Collectors;
 
 public class LedgerService 
 {
-	private static HashMap<String, LedgerMap> transactionToLedgerMap = new HashMap<String, LedgerMap>(){{
-		put("purchase" , LedgerMap.create()
-						   		.setCreditLedger("Principal")
-						   		.setDebitLedger("Cash-Out"));
-	}};
+    private static HashMap<String, LedgerMap> transactionToLedgerMap = new HashMap<String, LedgerMap>(){{
+	put("purchase" , LedgerMap.create()
+	                 .setCreditLedger("Principal")
+	                 .setDebitLedger("Cash-Out"));
+    }};
 
-	private static LedgerService ledgerServiceInstance;
-	private final LedgerDAO ledgerDAO;
+    private static LedgerService ledgerServiceInstance;
+    private final LedgerDAO ledgerDAO;
 
-	public LedgerService(LedgerDAO ledgerDAO) {
-		this.ledgerDAO = ledgerDAO;
+    public LedgerService(LedgerDAO ledgerDAO) {
+	this.ledgerDAO = ledgerDAO;
+    }
+
+    public static void createLedgerService(LedgerDAO ledgerDAO) {
+	ledgerServiceInstance = new LedgerService(ledgerDAO);
+    }
+
+    public static LedgerService getInstance() {
+	return ledgerServiceInstance;
+    }
+
+    public void createLedgers(Journal journal) {
+	LedgerMap cdmapping = transactionToLedgerMap.get(journal.getTransactionType().toLowerCase());
+	if(cdmapping != null ) {
+	    ledgerDAO.create(new Ledger(journal.getAccountId(),cdmapping.getCreditLedger(),journal.getAmount(),0));
+	    ledgerDAO.create(new Ledger(journal.getAccountId(),cdmapping.getDebitLedger(),0,journal.getAmount()));
 	}
+    }
 
-	public static void createLedgerService(LedgerDAO ledgerDAO) {
-		ledgerServiceInstance = new LedgerService(ledgerDAO);
-	}
-
-	public static LedgerService getInstance() {
-		return ledgerServiceInstance;
-	}
-
-	public void createLedgers(Journal journal) {
-		LedgerMap cdmapping = transactionToLedgerMap.get(journal.getTransactionType().toLowerCase());
-		if(cdmapping != null ) {
-			ledgerDAO.create(new Ledger(journal.getAccountId(),cdmapping.getCreditLedger(),journal.getAmount(),0));
-			ledgerDAO.create(new Ledger(journal.getAccountId(),cdmapping.getDebitLedger(),0,journal.getAmount()));
-		}
-	}
-
-	public double getSumOfPrincipal(long accountId) {
-		List<Ledger> principalLedgers = ledgerDAO.findByLedgerTypeAndAccountId("Principal", accountId);
-		return principalLedgers.stream().collect(Collectors.summingDouble(o -> o.getCredit()));
-	}
-
+    public double getSumOfPrincipal(long accountId) {
+	List<Ledger> principalLedgers = ledgerDAO.findByLedgerTypeAndAccountId("Principal", accountId);
+	return principalLedgers.stream().collect(Collectors.summingDouble(o -> o.getCredit()));
+    }
 }
